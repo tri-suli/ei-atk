58a872da31bfc85046fbada481ba06b920537c50
Tri Suli Prasetyo <trisulipras@gmail.com>
2021-04-02 19:45:25 +0700

    ADI-11: Create Login Page (#4)
    **WHAT**
    
    Before the user enters the main application page, a page is needed to check whether the user who will use the application has already been registered or not.
    
    **WHY**
    
    - Add new routes [/login]
    - Create login page blade
    - Create login form component
    - Create related tests

--- resources/views/theme.blade.php
+++ resources/views/theme.blade.php
@@ -1,22 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <!-- Meta, title, CSS, favicons, etc. -->
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="csrf-content" content="{{ csrf_field() }}" />
-
-    <title>{{ config('app.name') }} | {{ $page_name }}</title>
-
-    <link href="{{ asset('template/vendors/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('template/vendors/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('template/vendors/nprogress/nprogress.css') }}" rel="stylesheet">
-    @stack('vendor-styles')
-    <link href="{{ asset('template/build/css/custom.min.css') }}" rel="stylesheet">
-    @stack('custom-styles')
+    @include('components.themes.head')
   </head>
 
   <body class="nav-md">
